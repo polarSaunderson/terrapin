9fed58d83af6e331919b318742419ee6d9092b3a
polarSaunderson <Dominic.saunderson@monash.edu>
2023-09-07 23:07:28 +1000

    in `handle_monthDays` can strip out the year part of a date, and can handle different output formats

--- R/handle_monthDays.R
+++ R/handle_monthDays.R
@@ -1,8 +1,18 @@
-handle_monthDays <- function(x) {
+handle_monthDays <- function(x, out = "Jan-01", outSep = "-") {
   #' Reformat "month-day" information
   #'
   #' @description This function reformats month-day combinations to match
-  #'   "Jun-04".
+  #'   either "Jun-04", "04-Jun", "06-04", "04-06", "6-4", or "4-6". The
+  #'   last are ambiguous but can be useful in defined situations, for example
+  #'   pasting with a year to create a date.
+  #'
+  #'   ```
+  #'       Jun-04
+  #'       06-04
+  #'       04-06
+  #'       4-6
+  #'       6-4
+  #'   ````
   #'
   #'   This function should work as long as the month is unambiguous in a string
   #'   with a reasonable separator (see below). It can handle these inputs:
@@ -15,17 +25,33 @@ handle_monthDays <- function(x) {
   #'   The Jan can also be "jan", "January", or "jan".
   #'   ```
   #'   **note** This function cannot handle unambiguous combinations (e.g.
-  #'   02/01). It also doesn't (currently) strip out any year information in
-  #'   the date.
+  #'   02/01) as input. It can return them (for e.g. pasting with a year).
+  #'
+  #'   It can also strip the month-date out of a date if it is entered in the
+  #'   format YYYY-MM-DD or DD-MM-YYYY. But **NOT** MM-DD-YYYY.
   #'
   #' @param x A vector containing strings in which the month is clear. For a
   #'   list, use `handled <- lapply(x, handle_monthDays)`.
+  #' @param out How should the monthDays be returned? Choose from any of the
+  #'   following (using the examples of 20th June and 5th March). Note the
+  #'   ambiguity in the last options.
+  #'
+  #'   ```
+  #'       "Jan-01"   returns "Jun-20"   "Mar-05"
+  #'       "01-Jan"   returns "Jun-20"   "05-Mar"
+  #'       "mm-dd"    returns "20-06"    "03-05"
+  #'       "dd-mm"    returns "06-20"    "05-03"
+  #'       "m-d"      returns "6-20"     "3-5"
+  #'       "d-m"      returns "20-6"     "5-3"
+  #'   ```
+  #' @param outSep Which symbol should be placed between the date and month?
+  #'   Default is "-", but will accept any string.
   #'
   #' @export
 
   # Code -----------------------------------------------------------------------
   # Define function to call with lapply
-  handle_monthDays_internally <- function(y) {
+  handle_monthDays_internally <- function(y, out, md, outSep) {
     # Find just the monthly part
     yMonth <- get_months(y, "asis")
 
@@ -41,14 +67,97 @@ handle_monthDays <- function(x) {
       gsub(pattern = "/", replacement = "") |>
       gsub(pattern = "-", replacement = "") |>
       gsub(pattern = "_", replacement = "") |>
-      as.numeric() |>
-      sprintf(fmt = "%02d")
+      as.numeric()
 
-    # New date
-    z <- paste(get_months(yMonth, out = "Jan"), yDay, sep = "-")
+    # Handle for the formatting
+    if (out == "01" | out == 2) {
+      yDay <- sprintf(fmt = "%02d", yDay)
+      if (out == 2) {
+        out <- "Jan"
+      }
+    } else if (out == 3) {
+        out <- "Jan"
+    }
+
+    # Create the new date
+    if (isTRUE(md)) {
+      z <- paste(get_months(yMonth, out = out), yDay, sep = outSep)
+    } else if (isFALSE(md)) {
+      z <- paste(yDay, get_months(yMonth, out = out), sep = outSep)
+    }
     return(z)
   }
 
+  # Prepping for the internal function -----------------------------------------
+  strip_years <- function(x) {
+    # Year definition is based on 3 parts
+    xLength <- strsplit(x, "-") |> lapply(length) |> unlist()
+    xIsYear <- ifelse(test = (xLength == 3),
+                      yes  = TRUE,
+                      no   = FALSE)
+    xWhich  <- which(xIsYear == 1)
+    yYears  <- x[xWhich]
+
+    # Year part is defined by a length of 4 characters in position 1 or 3
+    ySplit  <- strsplit(yYears, "-")
+    yToSwap <- sapply(X = ySplit,
+                      FUN = function(ii){
+                        # Which position have 4 characters?
+                        iiLengths   <- lapply(ii, nchar) |> unlist()
+                        iiYearIndex <- which(iiLengths == 4)
+
+                        # Handle if the month had 4 characters (e.g. June, July)
+                        if (2 %in% iiYearIndex) {
+                          iiYearIndex <- iiYearIndex[-which(iiYearIndex == 2)]
+                        }
+
+                        # If multiple date parts have 4, there's an error!
+                        if (length(iiYearIndex) != 1) {
+                          stop("Incorrect date format entered. ",
+                               "Use YYYY-MM-DD or DD-MM-YYYY")
+                        }
+
+                        # Make month unambiguous
+                        ii[2] <- handle_months(ii[2], out = "Jan")
+
+                        # Remove the year part
+                        ii <- ii[-iiYearIndex]
+
+                        # Collapse down
+                        ii <- paste(ii, collapse = "-")
+
+                        return(ii)
+                      } )
+
+    # Replace any entries in x that had the year in
+    x[xWhich] <- yToSwap
+    return(x)
+  }
+
+  # Prepare for different output options
+  md  <- switch(tolower(out),
+                "mm-dd" = , "m-d" = , "mmdd" = , "md" = , "jan-1" = , "jan-01" = TRUE,
+                "dd-mm" = , "d-m" = , "ddmm" = , "dm" = , "1-jan" = , "01-jan" = FALSE,
+                TRUE)                       # default is Jan-01
+
+  out <- switch(tolower(out),
+                "mm-dd"  = , "dd-mm"  = , "ddmm"  = , "mmdd" = "01",
+                "m-d"    = , "d-m"    = , "dm"    = , "md"   = 1,
+                "jan-01" = , "01-jan" = 2,
+                "jan-1"  = , "1-jan"  = 3,  # will reset later on
+                2)                          # default is Jan-01
+
+  # Handle different date separators
+  x <- gsub(pattern = "/", replacement = "-", x) |>
+    gsub(pattern = "_", replacement = "-") |>
+    gsub(pattern = " ", replacement = "-")
+
+  # Remove any years in the dataset
+  x <- strip_years(x)
+
   # Apply the function
-  xOut <- sapply(x, handle_monthDays_internally)
+  xOut <- sapply(x, handle_monthDays_internally, out, md, outSep)
+
+  return(xOut)
+
 }
