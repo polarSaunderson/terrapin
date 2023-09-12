defe5a5d0bc24b1686a65a098c519bbf6395aa47
polarSaunderson <Dominic.saunderson@monash.edu>
2023-09-13 00:18:11 +1000

    changed dateList to periods and improved documentation in previous subset_by_date

--- R/subset_by_date.R
+++ R/subset_by_date.R
@@ -5,13 +5,13 @@ subset_by_date <- function(x,
   #'
   #' @description Easily select only layers of a SpatRaster that are on specific
   #'   dates. This function is distinct to `subset_by_day()` as it considers the
-  #'   full date, not just the day. It is usually quicker to chain together the
-  #'   "subset_by_x" functions unless an unusual combination of dates are
-  #'   required. See the examples.
+  #'   full date, not just the day. It is usually easier (and quicker) to chain
+  #'   together the "subset_by_x" functions unless an unusual combination of
+  #'   dates is required. See the examples.
   #'
   #'   The dates can be input either as a vector of specific dates using the
   #'   "dates" argument; or as a list of vectors containing the start and end
-  #'   dates for longer extended periods. All dates need to be entered in
+  #'   dates for longer extended 'periods'. All dates need to be entered in
   #'   YYYY-MM-DD format (e.g. "2019-12-31").
   #'
   #' @param x SpatRaster: The data to subset. Can be either a string, in which
@@ -26,12 +26,12 @@ subset_by_date <- function(x,
   #'   returned; if no dates match, an empty SpatRaster is returned).
   #'
   #' @param periods Used for extended periods rather than individual dates.
-  #'   Input must be a vector, with the first value indicating when the period
-  #'   begins and the second when the period ends. For multiple periods, use a
-  #'   list of such vectors, for example:
+  #'   Input dates must be a vector, with the first value indicating when the
+  #'   period begins and the second when the period ends. For multiple periods,
+  #'   use a list of such vectors, for example:
   #'      ```
-  #'      list(c("2019-01-01", "2019-01-03"),
-  #'           c("2011-03-08", "2011-03-11"))
+  #'      list(c("2019-01-01", "2019-01-03"),     # 1-3  Jan 2019
+  #'           c("2011-03-08", "2011-03-11"))     # 8-11 Mar 2011
   #'      ````
   #'   The above will return the layers for 1st, 2nd and 3rd of January 2019,
   #'   and the 8th, 9th, 10th and 11th of March 2011.
@@ -44,50 +44,49 @@ subset_by_date <- function(x,
   #'   xSubset1 <- subset_by_date(x, dates = c("1991-12-01", "1994-12-19",
   #'                                           "2003-04-17", "2012-09-22"))
   #'
-  #'   # For extended periods, use the "dateList" argument:
-  #'   xSubset2 <- subset_by_date(x, dateList = list(c("1991-12-01", "1991-12-05"),
-  #'                                                 c("1993-11-15", "1993-12-15")))
+  #'   # For extended periods, use the "periods" argument:
+  #'   xSubset2 <- subset_by_date(x, periods = list(c("1991-12-01", "1991-12-05"),
+  #'                                                c("1993-11-15", "1993-12-15")))
   #'
-  #'   # However, it can be easier to chain other "subset_by_x" functions:
-  #'   xSubset3 <- subset_by_year(x, 1991) |>
-  #'               subset_by_month(12)
+  #'   # However, it can be easier to chain other "subset_by_x" functions.
+  #'   # The following two are equivalent.
+  #'   xSubset3 <- subset_by_year(x, 1991) |> subset_by_month(12)
   #'
-  #'   # instead of using subset_by_date:
-  #'   xSubset4 <- subset_by_date(x, dateList = list(c("1991-12-01", "1991-12-31")
+  #'   xSubset4 <- subset_by_date(x, periods = c("1991-12-01", "1991-12-31"))
   #' }
   #'
   #' @export
 
   # Code -----------------------------------------------------------------------
   # Check that dates have been requested!
-  if (is.null(dates[1]) & is.null(dateList[1])) {
+  if (is.null(dates[1]) & is.null(periods[1])) {
     stop("No dates selected!")
   }
-  if (!is.null(dates[1]) & !is.null(dateList[1])) {
+  if (!is.null(dates[1]) & !is.null(periods[1])) {
     stop("Please use either the dates or periods argument, not both.")
   }
 
-  # Handle if x is a filename
-  if ("SpatRaster" %notIn% methods::is(x)) {
-    x <- terra::rast(x)
-  }
-
-  # Format the dates as necessary - account for using dates or dateList argument
-  if (!is.null(dateList)) {
+  # Format the dates as necessary - account for using dates or periods argument
+  if (!is.null(periods)) {
     # Handle if a single vector was supplied
-    if (!is.list(dateList)) dateList <- list(dateList)
+    if (!is.list(periods)) periods <- list(periods)
 
-    # If using the dateList argument
+    # If using the periods argument
     dates <- c() # preallocate
 
     # extend date periods and add
-    for (ii in seq_along(dateList)) {
-      iiDates <- as.Date(dateList[[ii]][1]):as.Date(dateList[[ii]][2])
+    for (ii in seq_along(periods)) {
+      iiDates <- as.Date(periods[[ii]][1]):as.Date(periods[[ii]][2])
       dates   <- c(dates, iiDates)
     }
     dates <- as.Date(dates, "1970-01-01") |>
       as.character()                           # format as string of dates
   }
+
+  xSubset <- subset_by(x, type = "date", )
+
+  # Handle if x is a filename
+  x <- terra::rast(x, keeptime = TRUE, keepunits = TRUE, props = TRUE)
 
   # Get all dates of the input
   xDates <- get_terra_dates(x, australSplit = NULL)
